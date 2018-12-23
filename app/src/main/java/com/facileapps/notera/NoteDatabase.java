55f054357702814e6502b4689fac8aecad16958d
Felipe <felipezb@live.com>
2018-12-23 18:48:54 +0200

    Added Repository and initial db data

--- app/src/main/java/com/facileapps/notera/NoteDatabase.java
+++ app/src/main/java/com/facileapps/notera/NoteDatabase.java
@@ -1,10 +1,15 @@
 package com.facileapps.notera;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import java.util.Date;
+
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Note.class, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -18,9 +23,35 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
 
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDatabaseAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDatabaseAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private NoteDao noteDao;
+
+        private PopulateDatabaseAsyncTask(NoteDatabase noteDatabase) {
+            this.noteDao = noteDatabase.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("First Note",
+                    "This is the first note, fell free to delete, edit it and create your own notes",
+                    new Date()));
+            return null;
+        }
     }
 }
